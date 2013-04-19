95ad58a77fad021780c6e7f663fc91688ecdd125
MaxBobisch <max.bobisch@gmx.de>
2013-04-19 14:18:35 +0200

    Phase 2 angelegt

--- Phase1/src/rezeptSammlung/XMLHandler.java
+++ Phase1/src/rezeptSammlung/XMLHandler.java
@@ -6,7 +6,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
-import java.util.List;
 import java.util.ListIterator;
 import java.util.Scanner;
 
